8b79ba55d97ae65ee5234c8baaa29b56bafc34f8
Raul Costas <raulcostasdel@gmail.com>
2026-08-11 22:21:10 -0400

    Actualizar puerto por defecto de PostgreSQL a 5432

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -142,7 +142,7 @@ import { ConsentimientoPaciente } from './consentimientos-pacientes/entities/con
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5433', 10),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USER || process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'postgrespg',
       database: process.env.DB_NAME || 'curare',
